46c7786f0a7719b8a75d6835f785c42554a09502
Thiago Reis Nascimento <trnasci@gmail.com>
2023-02-09 14:51:30 -0300

    Alterando arquivo connecton.ts

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -7,5 +7,4 @@ export default mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.MYSQL_DATABASE || 'Trybesmith',
 });
